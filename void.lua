e5a2f39c82fd170ca34481ae4ae848b70c747ae2
Associable <110120454+Associable@users.noreply.github.com>
2023-05-21 11:33:38 -0500

    Update void.lua

--- void.lua
+++ void.lua
@@ -1,1 +1,1 @@
-https://api.luarmor.net/files/v3/loaders/6931bb782adf456327229e8c56754af0.lua
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6931bb782adf456327229e8c56754af0.lua"))()
